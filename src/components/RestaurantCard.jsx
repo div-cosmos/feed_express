29d71264adaa9da6bf8f3f7256eb201c54b16490
Divyanshu <divyanshuverma919@gmail.com>
2024-04-30 04:33:48 +0530

    integrated swiggy's restaurant list API

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,8 +1,15 @@
 import { IMG_CDN_URL } from "../constants";
 
 const RestaurantCard = (props) => {
-  const { name, cuisines, resBanner, deliveryTime, costForTwo, avgRating } =
-    props;
+  const {
+    name,
+    cuisines,
+    resBanner,
+    deliveryTime,
+    costForTwo,
+    avgRating,
+    areaName,
+  } = props;
 
   return (
     <div className="res-card">
@@ -13,11 +20,13 @@ const RestaurantCard = (props) => {
           alt="res-banner"
         />
       </div>
-      <h3>{name}</h3>
-      <h4>{cuisines.join(", ")}</h4>
-      <h4>{avgRating} stars</h4>
-      <h4>{costForTwo / 100} FOR TWO</h4>
-      <h4>{deliveryTime}minutes</h4>
+      <h4>{name}</h4>
+      <h4>
+        {avgRating} stars {deliveryTime ?? "-"} mins
+      </h4>
+      <h4>{costForTwo}</h4>
+      <p>{cuisines?.join(", ")}</p>
+      <p>{areaName}</p>
     </div>
   );
 };
--- src/layouts/AppLayout/AppLayout.jsx
+++ src/layouts/AppLayout/AppLayout.jsx
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 import RestaurantCard from "../../components/RestaurantCard";
-import { restaurantList } from "../../constants";
 import "./AppLayout.css";
 
 const AppLayout = () => {
-  const [restaurantInfoList, setRestaurantList] =
-    React.useState(restaurantList);
+  const [restaurantInfoList, setRestaurantList] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      `https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`
+    );
+
+    const jsonData = await data.json();
+    console.log(jsonData);
+
+    setRestaurantList(() => {
+      let restaurantData = [];
+      jsonData.data?.cards?.map((card) => {
+        if (card?.card?.card?.id === "restaurant_grid_listing") {
+          restaurantData = card?.card?.card?.gridElements?.infoWithStyle
+            ?.restaurants?.length
+            ? card?.card?.card?.gridElements?.infoWithStyle?.restaurants
+            : {};
+        }
+      });
+      return restaurantData?.length ? restaurantData : [];
+    });
+  };
+
+  /* Event Handlers */
   const onTopRatedResFilter = () => {
     setRestaurantList(
-      restaurantInfoList.filter((resList) => {
-        if (!isNaN(resList.data?.avgRating))
-          return Number(resList.data.avgRating) > 4;
-        else return resList;
+      restaurantInfoList.filter((resInfo) => {
+        if (!isNaN(resInfo.info?.avgRating))
+          return Number(resInfo.info?.avgRating) > 4;
+        else return resInfo;
       })
     );
   };
@@ -23,15 +48,27 @@ const AppLayout = () => {
     /* Sectional Renders */
   }
   const renderRestaurantCards = () => {
-    return restaurantInfoList.map((restaurantItem) => {
-      const dynamicDataAsProps = {
-        name: restaurantItem.data?.name,
-        cuisines: restaurantItem.data?.cuisines,
-        resBanner: restaurantItem.data?.cloudinaryImageId,
-        deliveryTime: restaurantItem.data?.deliveryTime,
-        costForTwo: restaurantItem.data?.costForTwo,
-        avgRating: restaurantItem.data?.avgRating,
-      };
+    return restaurantInfoList?.map((restaurantItem) => {
+      let dynamicDataAsProps = {};
+      if (restaurantItem?.info) {
+        dynamicDataAsProps.name = restaurantItem?.info?.name;
+        dynamicDataAsProps.cuisines = restaurantItem?.info?.cuisines;
+        dynamicDataAsProps.resBanner = restaurantItem?.info?.cloudinaryImageId;
+        dynamicDataAsProps.deliveryTime = restaurantItem?.info?.deliveryTime;
+        dynamicDataAsProps.costForTwo = restaurantItem?.info?.costForTwo;
+        dynamicDataAsProps.avgRating = restaurantItem?.info?.avgRating;
+        dynamicDataAsProps.areaName = restaurantItem?.info?.areaName;
+      }
+
+      // else {
+      //   dynamicDataAsProps.name = restaurantItem.data?.name;
+      //   dynamicDataAsProps.cuisines = restaurantItem.data?.cuisines;
+      //   dynamicDataAsProps.resBanner = restaurantItem.data?.cloudinaryImageId;
+      //   dynamicDataAsProps.deliveryTime = restaurantItem.data?.deliveryTime;
+      //   dynamicDataAsProps.costForTwo = restaurantItem.data?.costForTwo;
+      //   dynamicDataAsProps.avgRating = restaurantItem.data?.avgRating;
+      // }
+
       return (
         <RestaurantCard
           key={restaurantItem.data?.uuid}
